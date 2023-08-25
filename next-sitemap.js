21a51ee476386350379389afca2829bca6116625
sam bacha <sam@manifoldfinance.com>
2023-08-25 06:04:47 -0700

    feat(nextjs): sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,11 @@
+// @ts-check
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.meveth.org',
+  generateRobotsTxt: true,
+  // optional
+  robotsTxtOptions: {
+    additionalSitemaps: [
+    ],
+  },
+}
