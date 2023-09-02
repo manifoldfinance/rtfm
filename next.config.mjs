4bc6d4a0bf064c1835a39735efe0117acd11aed5
sam bacha <sam@manifoldfinance.com>
2023-09-02 09:41:46 +0200

    refactor(sdks): update images and add solidity (#9)

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@
  * @type {import('next').NextConfig}
  */
 
+import nextMDX from '@next/mdx';
 
-import nextMDX from '@next/mdx'
-
-import { recmaPlugins } from './src/mdx/recma.mjs'
-import { rehypePlugins } from './src/mdx/rehype.mjs'
-import { remarkPlugins } from './src/mdx/remark.mjs'
-import withSearch from './src/mdx/search.mjs'
+import { recmaPlugins } from './src/mdx/recma.mjs';
+import { rehypePlugins } from './src/mdx/rehype.mjs';
+import { remarkPlugins } from './src/mdx/remark.mjs';
+import withSearch from './src/mdx/search.mjs';
 
 const withMDX = nextMDX({
   options: {
@@ -17,12 +16,12 @@ const withMDX = nextMDX({
     rehypePlugins,
     recmaPlugins,
   },
-})
+});
 
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+};
 
-export default withSearch(withMDX(nextConfig))
+export default withSearch(withMDX(nextConfig));
