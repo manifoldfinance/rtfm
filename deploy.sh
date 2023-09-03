d603d8641bb145e95b498e04e18741b2cc8afe5d
sam bacha <sam@manifoldfinance.com>
2023-09-03 04:05:13 -0700

    build(testing): init instrumention

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 export NODE_ENV=production
 export NEXT_TELEMETRY_DISABLED=1
 
-export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
-export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
-export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
+#export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
+#export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
+#export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
 BUILD_HASH_VERSION=$(git rev-parse --short HEAD 2>/dev/null || find ./* -type f -name '*.go' -print0 | sort -z | xargs -0 sha1sum | sha1sum | sed -r 's/[^\da-f]+//g')
 
 # shellcheck disable=SC2034
@@ -16,28 +16,27 @@ echo "$BUILD_HASH_VERSION"
 echo "VERCEL_ENV: $VERCEL_ENV"
 
 vercel() {
-if [[ "$VERCEL_ENV" == "production" ]] ; then
-  # Proceed with the build
-  echo "✅ - Build can proceed"
+  if [[ "$VERCEL_ENV" == "production" ]]; then
+    # Proceed with the build
+    echo "✅ - Build can proceed"
     npx next build
     sleep 1
     npm run postbuild
     sleep 1
-  exit 0;
+    exit 0
 
-else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 1;
-fi
-};
+  else
+    # Don't build
+    echo "🛑 - Build cancelled"
+    exit 1
+  fi
+}
 
-if [ "$1" = "production" ]
-then
-  vercel
+if [ "$1" = "local" ]; then
+  echo "🚀 - Deploying"
+  vercel deploy --prod
 fi
 
 "$1"
 echo "🆘 - ERROR: Command not found"
 exit 127
-
